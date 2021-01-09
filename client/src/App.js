30a7c196eb42b348335d38c2f32c589bcdf566a1
User <user@Users-MacBook-Pro.local>
2021-01-09 10:54:52 +1000

    initial commit of repos

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
